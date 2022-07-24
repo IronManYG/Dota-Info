6c516742d5169ee1bc303d882d7773d55cf3db29
IronManYG <hussain.yg2010@gmail.com>
2022-07-24 15:38:31 +0300

    Add Dependencies and buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath(Build.androidBuildTools)
+        classpath(Build.kotlinGradlePlugin)
     }
 }
 
