5f082ddf6f707e6b518c6a95c8f49ac96477244c
IronManYG <hussain.yg2010@gmail.com>
2022-07-25 14:06:24 +0300

    GetHeros Use-case

---
+++ hero/hero-interactors/src/main/java/com/example/hero_interactors/GetHeros.kt
@@ -0,0 +1,49 @@
+package com.example.hero_interactors
+
+import com.example.core.DataState
+import com.example.core.ProgressBarState
+import com.example.core.UIComponent
+import com.example.hero_domain.Hero
+import com.example.hero_domain.network.HeroService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetHeros(
+    // TODO(Add caching)
+    private val service: HeroService,
+) {
+
+    fun execute(): Flow<DataState<List<Hero>>> = flow {
+        try {
+            emit(DataState.Loading(progressBarState = ProgressBarState.Loading))
+
+            val heros: List<Hero> = try { // catch network exceptions
+                service.getHeroStats()
+            }catch (e: Exception){
+                e.printStackTrace() // log to crashlytics?
+                emit(DataState.Response<List<Hero>>(
+                    uiComponent = UIComponent.Dialog(
+                        title = "Network Data Error",
+                        description = e.message?: "Unknown error"
+                    )
+                ))
+                listOf()
+            }
+
+            // TODO(Add caching)
+
+            emit(DataState.Data(heros))
+        }catch (e: Exception){
+            e.printStackTrace()
+            emit(DataState.Response<List<Hero>>(
+                uiComponent = UIComponent.Dialog(
+                    title = "Error",
+                    description = e.message?: "Unknown error"
+                )
+            ))
+        }
+        finally {
+            emit(DataState.Loading(progressBarState = ProgressBarState.Idle))
+        }
+    }
+}
---
+++ hero/hero-interactors/src/main/java/com/example/hero_interactors/HeroInteractors.kt
@@ -0,0 +1,19 @@
+package com.example.hero_interactors
+
+import com.example.hero_domain.network.HeroService
+
+data class HeroInteractors(
+    val getHeros: GetHeros,
+    // TODO(Add other hero interactors)
+) {
+    companion object Factory {
+        fun build(): HeroInteractors{
+            val service = HeroService.build()
+            return HeroInteractors(
+                getHeros = GetHeros(
+                    service = service,
+                ),
+            )
+        }
+    }
+}
