aa5a18bfca150a59f1276ae58265be39560d595c
IronManYG <hussain.yg2010@gmail.com>
2022-07-26 15:10:26 +0300

    Retrieving a Hero from Cache

---
+++ hero/hero-interactors/src/main/java/com/example/hero_interactors/GetHeroFromCache.kt
@@ -0,0 +1,46 @@
+package com.example.hero_interactors
+
+import com.example.core.DataState
+import com.example.core.ProgressBarState
+import com.example.core.UIComponent
+import com.example.hero_datasource.cache.HeroCache
+import com.example.hero_domain.Hero
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+/**
+ * Retrieve a hero from the cache using the hero's unique id
+ */
+class GetHeroFromCache(
+    private val cache: HeroCache
+) {
+
+    fun execute(
+        id: Int,
+    ): Flow<DataState<Hero>> = flow {
+        try {
+            emit(DataState.Loading(progressBarState = ProgressBarState.Loading))
+
+            // emit data from cache
+            val cachedHero = cache.getHero(id)
+
+            if (cachedHero == null) {
+                throw Exception("That hero does not exist in the cache.")
+            }
+
+            emit(DataState.Data(cachedHero))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(
+                DataState.Response<Hero>(
+                    uiComponent = UIComponent.Dialog(
+                        title = "Error",
+                        description = e.message ?: "Unknown error"
+                    )
+                )
+            )
+        } finally {
+            emit(DataState.Loading(progressBarState = ProgressBarState.Idle))
+        }
+    }
+}
